f7d05ef60b964fa0e73773669ffaed6214e31ec5
Shadowbee <zaun.bein@web.de>
2024-11-16 22:12:58 +0100

    bumped up version to 1.1.1, added a lot item options

--- MC_calculator/src/menus.rs
+++ MC_calculator/src/menus.rs
@@ -14,12 +14,20 @@ pub fn portal_input() {
     }
 }
 pub fn item_operations_input() {
-    println!("Enter:\n 1 => stacks to items\n 2 => items to stacks \n 3 => menu");
+    println!("Enter:\n 1 => stacks to items\n 2 => items to stacks\n 3 => items to shulkers\n 4 => Shulker to items\n 5 => items_to_dchests\n 6 => dchests_to_items\n 7 => stacks_to_dchests\n 8 => dchests_to_stacks\n 9 => dchests_to_fullshulker\n 10 => fullshulker_to_dchests\n 11 => menu");
     let doing = input::menu_input();
     match doing.trim() {
         "1" => item_opperations::stack_to_items(),
         "2" => item_opperations::items_to_stack(),
-        "3" => main(),
+        "3" => item_opperations::items_to_shulker(),
+        "4" => item_opperations::shulker_to_items(),
+        "5" => item_opperations::items_to_dchests(),
+        "6" => item_opperations::dchests_to_items(),
+        "7" => item_opperations::stacks_to_dchests(),
+        "8" => item_opperations::dchests_to_stacks(),
+        "9" => item_opperations::dchests_to_fullshulker(),
+        "10" => item_opperations::fullshulker_to_dchests(),
+        "11" => main(),
         _ => {
             println!("Invalid Input");
             item_operations_input()
--- MC_calculator/src/opperations/item_opperations.rs
+++ MC_calculator/src/opperations/item_opperations.rs
@@ -14,3 +14,59 @@ pub fn items_to_stack() {
     println!("the stack count is {}", sc);
     menus::item_operations_input()
 }
+pub fn items_to_shulker() {
+    println!("Please enter the item count that you want to turn into shulkerboxes");
+    let mut sc = input::storage_input();
+    sc /= 1728.0;
+    println!("the shulker count is {}", sc);
+    menus::item_operations_input()
+}
+pub fn shulker_to_items() {
+    println!("Please enter the shulker count that you want to turn into items");
+    let mut sc = input::storage_input();
+    sc *= 1728.0;
+    println!("the item count is {}", sc);
+    menus::item_operations_input()
+}
+pub fn items_to_dchests() {
+    println!("Please enter the shulker count that you want to turn into items");
+    let mut sc = input::storage_input();
+    sc /= 3456.0;
+    println!("the item count is {}", sc);
+    menus::item_operations_input()
+}
+pub fn dchests_to_items() {
+    println!("Please enter the shulker count that you want to turn into items");
+    let mut sc = input::storage_input();
+    sc *= 3456.0;
+    println!("the item count is {}", sc);
+    menus::item_operations_input()
+}
+pub fn stacks_to_dchests() {
+    println!("Please enter the shulker count that you want to turn into items");
+    let mut sc = input::storage_input();
+    sc /= 54.0;
+    println!("the item count is {}", sc);
+    menus::item_operations_input()
+}
+pub fn dchests_to_stacks() {
+    println!("Please enter the shulker count that you want to turn into items");
+    let mut sc = input::storage_input();
+    sc *= 54.0;
+    println!("the item count is {}", sc);
+    menus::item_operations_input()
+}
+pub fn dchests_to_fullshulker() {
+    println!("Please enter the shulker count that you want to turn into items");
+    let mut sc = input::storage_input();
+    sc *= 93312.0;
+    println!("the item count is {}", sc);
+    menus::item_operations_input()
+}
+pub fn fullshulker_to_dchests() {
+    println!("Please enter the shulker count that you want to turn into items");
+    let mut sc = input::storage_input();
+    sc /= 93312.0;
+    println!("the item count is {}", sc);
+    menus::item_operations_input()
+}
