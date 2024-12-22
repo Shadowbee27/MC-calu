f7586ecae0b49f9cb9e82eabde84d6ab5d5eca4a
Shadowbee <zaun.bein@web.de>
2024-12-22 11:49:20 +0100

    Corrected Gramma

--- src/opperations/item_opperations.rs
+++ src/opperations/item_opperations.rs
@@ -1,16 +1,16 @@
-use crate::{input, lib, menus};
+use crate::{lib, menus};
 
 pub fn division(divisor: f64, base:String, result:String) {
     println!("Please enter the amount of {base} that you want to convert to {result}");
     let dividend = lib::input::storage_input();
     let quotient = dividend/divisor;
-    println!("The {result} size is {quotient}");
+    println!("The amount of {result} is {quotient}");
     menus::item_operations_input()
 }
 pub fn multiplication(factor1:f64, base:String, result:String) {
     println!("Please enter the amount of {base} that you want to convert to {result}");
     let factor2 = lib::input::storage_input();
     let product = factor1*factor2;
-    println!("The {result} size is {product}");
+    println!("The amount of {result} is {product}");
     menus::item_operations_input()
 }
